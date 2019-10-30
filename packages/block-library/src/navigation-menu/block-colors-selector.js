fc42e1f44a4c6054292df6f9981c65675f431b20
retrofox <rdsuarez@gmail.com>
2019-10-30 17:57:42 -0300

    colors-selector: fix setting bgcolor in Aa icon

--- packages/block-library/src/navigation-menu/block-colors-selector.js
+++ packages/block-library/src/navigation-menu/block-colors-selector.js
@@ -29,6 +29,8 @@ const ColorSelectorIcon = ( { backgroundColor, textColor } ) => {
 	}
 
 	const iconClasses = classnames( 'block-library-colors-selector__state-selection', {
+		'has-background-color': backgroundColor && backgroundColor.color,
+		'has-text-color': backgroundColor && backgroundColor.color,
 		[ backgroundColor.class ]: backgroundColor && backgroundColor.class,
 		[ textColor.class ]: textColor && textColor.class,
 	} );
